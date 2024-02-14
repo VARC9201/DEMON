51b8be128ee88350369ef09790a3d40746e4f565
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-02-14 20:58:23 +0530

    Update ping.py

--- DEADLYSPAM/plugins/ping.py
+++ DEADLYSPAM/plugins/ping.py
@@ -23,4 +23,4 @@ async def ping(event):
         text = await event.reply(f"» ᴘᴏɴɢ!", parse_mode=None, link_preview=None)
         end = datetime.now()
         result = (end - start).microseconds / 1000
-        await text.edit(f"» ᴅᴇᴀᴅʟʏ ꜱᴘᴀᴍʙᴏᴛ 🔥\n\n💫 ᴘᴏɴɢ: {result} ms🥀`")
+        await text.edit(f"» ᴅᴇᴍᴏɴ-ꜱᴘᴀᴍ ʙᴏᴛ 🕊\n\n💫 ᴘᴏɴɢ: {result} ms🥀`")
