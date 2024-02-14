2a50c86274ae09a58bda1aabe8f84813d403d2f8
ᴋᴜɴᴀʟ [AFK] <155554911+VARC9210@users.noreply.github.com>
2024-02-14 21:16:14 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -8,15 +8,14 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH")
-
-# ADMIN DETAILS (Your ID) 
+ 
 OWNER_ID = int(getenv("OWNER_ID", ""))
 
 OWNER_NAME = getenv("OWNER_NAME") 
 SUDO_USER= list(
-    map(int, getenv("SUDO_USER", "5486520312").split())
+    map(int, getenv("SUDO_USER", "6257927828").split())
 )
-# BOT TOKEN CONFIG VARS (get all vars detail from @botfather) 
+
 BOT_TOKEN = getenv("BOT_TOKEN", None) 
 BOT_TOKEN2 = getenv("BOT_TOKEN2", None) 
 BOT_TOKEN3 = getenv("BOT_TOKEN3", None) 
@@ -28,16 +27,8 @@ BOT_TOKEN8 = getenv("BOT_TOKEN8", None)
 BOT_TOKEN9 = getenv("BOT_TOKEN9", None) 
 BOT_TOKEN10 = getenv("BOT_TOKEN10", None) 
 
-
-# EXTRA VARS
 ALIVE_PIC = getenv("ALIVE_PIC") 
 CMD_HNDLR = getenv("CMD_HNDLR") 
-
-
-
-
-
-
 
 LUND = list(
     map(int, getenv("LUND", "123456789").split())
